8e59a86dacd1c6fa79cdc829a3c6946a604e4c22
Stuart Mumford <s.mumford@sheffield.ac.uk>
2013-11-19 19:12:44 +0000

    gdf writer ib

--- pysac/io/gdf_writer.py
+++ pysac/io/gdf_writer.py
@@ -406,12 +406,15 @@ def write_field(gdf_file, field, field_shape=None, arr_slice=np.s_[:]):
     if not field_shape:
         field_shape = field['field'].shape
         
-    gr.create_dataset(field['field_title'], field_shape, dtype='d')
+    dset = gr.create_dataset(field['field_title'], field_shape, dtype='d')
     
     fv = gdf_file['field_types'].create_group(field['field_title'])
     fv.attrs['field_name'] = field['field_name']
     fv.attrs['field_to_cgs'] = field['field_to_cgs']
     fv.attrs['field_units'] = field['field_units'] 
     fv.attrs['staggering'] = field['staggering']
-   
-    gr[field['field_title']][arr_slice] = np.array(field['field'])
+    
+    s = [arr_slice[0].start,arr_slice[1].start,arr_slice[2].start]
+    e = [arr_slice[0].stop,arr_slice[1].stop,arr_slice[2].stop]
+
+    #gr[field['field_title']][arr_slice] = np.array(field['field'])
