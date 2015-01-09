92cbf5123c46037b9e1b8239a6c806481a563902
Stuart Mumford <stuart@mumford.me.uk>
2015-01-09 18:24:15 +0000

    Update data files and read in using Table.

--- pysac/mhs_atmosphere/hs_model/hs_atmosphere.py
+++ pysac/mhs_atmosphere/hs_model/hs_atmosphere.py
@@ -4,149 +4,135 @@ Created on Thu Dec 11 11:37:39 2014
 
 @author: sm1fg
 
-    Generate a 1D non-magnetic atmosphere vector based on an empirical model 
-    based on observational data, or specify an analytical hydrostatic 
+    Generate a 1D non-magnetic atmosphere vector based on an empirical model
+    based on observational data, or specify an analytical hydrostatic
     equilibrium atmosphere.
 
 """
 import numpy as np
 from scipy.interpolate import UnivariateSpline
 
+import astropy.table
+from astropy.table import Table
+import astropy.units as u
+from astropy.constants import k_B, m_p
+
+__all__ = ['read_VAL3c_MTW', 'interpolate_atmosphere', 'get_spruit_hs', 'vertical_profile']
+
 #============================================================================
 # Read in and interpolate HD atmosphere
 #============================================================================
-def interpolate_atmosphere(filenames, 
-                           Z, 
-                           scales, 
-                           model_pars, 
-                           physicalconstants, 
-                           logical_pars, 
-                           plot=False):
 
+def read_VAL3c_MTW(VAL_file=None, MTW_file=None, mu=0.6):
+    """
+    Read in the data from Table 12 in Vernazza (1981) and combine with
+    McWhirter (1975).
+
+    Parameters
+    ----------
+    VAL_file : string
+        The data file for the VAL3c atmosphere, defaults to
+        `pysac.mhs_atmosphere.hs_model.VALIIIc_data`
+
+    MTW_file : string
+        The data file for the McWhirter atmosphere, defaults to
+        `pysac.mhs_atmosphere.hs_model.MTWcorona_data`
+
+    mu : float
+        The mean molecular weight ratio for the corona. defaults to 0.6.
+
+    Returns
+    -------
+    data : `astropy.table.Table`
+        The combined data, sorted by Z.
+    """
+    from . import VALIIIc_data, MTWcorona_data
+    if not VAL_file:
+        VAL_file = VALIIIc_data
+    if not MTW_file:
+        MTW_file = MTWcorona_data
+
+    VAL3c = Table.read(VAL_file, format='ascii', comment='#')
+    VAL3c['Z'].unit = u.km
+    VAL3c['rho'].unit = u.Unit('g cm-3')
+    VAL3c['p'].unit = u.Unit('dyne/cm^2')
+    VAL3c['T'].unit = u.K
+    VAL3c['n_i'].unit = u.one/u.cm**3
+    VAL3c['n_e'].unit = u.one/u.cm**3
+
+    # Calculate the mean molecular weight ratio
+    VAL3c['mu'] = 4.0/(3*0.74+1+VAL3c['n_e'].quantity/VAL3c['n_i'].quantity)
+
+    MTW = Table.read(MTW_file, format='ascii', comment='#')
+    MTW['Z'].unit = u.km
+    MTW['T'].unit = u.K
+    MTW['p'].unit = u.Unit('dyne/cm^2')
+    MTW['rho'] = (MTW['p'] / k_B / MTW['T'] * m_p * mu).to('g cm-3')
+
+    MTW['mu'] = mu
+
+    data = astropy.table.vstack([VAL3c, MTW], join_type='inner')
+    data.sort('Z')
+
+    return data
+
+def interpolate_atmosphere(data, Z):
     """ This module generates a 1d array for the model plasma preesure, plasma
-    density, temperature and mean molecular weight. 
+    density, temperature and mean molecular weight.
     """
-#----------------------------------------------------------------------------
-    if logical_pars['l_atmos_val3c_mtw']:
-        """
-    "filenames": a list of strings containing the paths of model data 
-    for this module we combine data from Table 12 in Vernazza 1981 with
-    McWhirter 1975 and convert units to SI
-    In Vernazza data is:
-        H(km) | \rho (g cm^-3) | Ptot (dyn cm^-2) | T (K) | Pgas / Ptot
-    and in McWhirter 1975 data is:  
-        H(km) | T (K)          | Ptot (dyn cm^-2) |
-    The data is interpolated onto an array in Z applicable to the model
-    resolution
-    """
-        VAL3c = np.loadtxt(filenames[0])[::-1,:]
-        VAL3c[:,0] *= 1e3 #km -> m
-        VAL3c[:,1] *= 1e3 #g/cm^3 -> kg/m^3
-        VAL3c[:,2] /= 1e1 #dyne/cm^2 -> Pascals(N/m^2)
-        muTV=4.0/(3*0.74+1+VAL3c[:,6]/VAL3c[:,5])
-        nzv=VAL3c[:,0].size
-        MTW =np.loadtxt(filenames[1])[::-1,:]
-        MTW[:,0] *= 1e3 #km -> m
-        MTW[:,1] *= 1   #Kelvin
-        MTW[:,2] /= 1e1 #dyne/cm^2 -> Pascals(N/m^2)
-        nzm=MTW[:,0].size
-        #Combine both sets into single array
-        hdata=np.zeros(nzv+nzm)
-        hdata[0:nzv]=VAL3c[:,0]
-        hdata[nzv:nzv+nzm]=MTW[:,0]
-        hdata /= scales['length']
-    
-        pdata=np.zeros(nzv+nzm)
-        pdata[0:nzv]=VAL3c[:,2]
-        pdata[nzv:nzv+nzm]=MTW[:,2]
-        pdata /= scales['energy density']
-    
-        Tdata=np.zeros(nzv+nzm)
-        Tdata[0:nzv]=VAL3c[:,3]
-        Tdata[nzv:nzv+nzm]=MTW[:,1] 
-        Tdata /= scales['temperature']
-    
-        rdata=np.zeros(nzv+nzm)
-        rdata[0:nzv]=VAL3c[:,1]
-        rdata /= scales['density']   
-        #MTW[:,2], kB, mp and mu are in code units so no rescale needed
-        rdata[nzv:nzv+nzm] = MTW[:,2]/physicalconstants['boltzmann']/MTW[:,1] \
-                        *physicalconstants['proton_mass']*physicalconstants['mu']
-    
-        muofT = np.zeros(nzv+nzm) # mean molecular weight
-        muofT[0:nzv] = muTV
-        muofT[nzv:nzv+nzm] = physicalconstants['mu']
-        # interpolate total pressure, temperature and density profiles
-        pdata_f = UnivariateSpline(hdata,np.log(pdata),k=1, s=0.25)
-        Tdata_f = UnivariateSpline(hdata,np.log(Tdata),k=1, s=0.25)
-        rdata_f = UnivariateSpline(hdata,np.log(rdata),k=1, s=0.25)
-        #s=0.0 to ensure all points are strictly used for ionisation state
-        muofT_f = UnivariateSpline(hdata,np.log(muofT),k=1, s=0.0)
-        pdata_i = np.exp(pdata_f(Z))
-        Tdata_i = np.exp(Tdata_f(Z))
-        rdata_i = np.exp(rdata_f(Z))
-        muofT_i = np.exp(muofT_f(Z))
 
-        source_data = [VAL3c,MTW]                          
-        if plot:
-            return pdata_i, Tdata_i, rdata_i, muofT_i, source_data
-        else:        
-            return pdata_i, Tdata_i, rdata_i, muofT_i
-#----------------------------------------------------------------------------
-# a simpler exponential atmosphere to test Spruit's analytical result
-#----------------------------------------------------------------------------
-    if logical_pars['l_spruit']:
-        pdata_i, Tdata_i, rdata_i, muofT_i, VAL3c, MTW\
-               = get_spruit_hs(
-                               filenames, 
-                               Z, 
-                               scales, 
-                               model_pars,
-                               physicalconstants,
-                               logical_pars,
-                               plot
-                              )
-        source_data = [VAL3c,MTW]                          
-        if plot:
-            return pdata_i, Tdata_i, rdata_i, muofT_i, source_data
-        else:        
-            return pdata_i, Tdata_i, rdata_i, muofT_i
+    hdata = data['Z'].quantity.value
+    # interpolate total pressure, temperature and density profiles
+    pdata_f = UnivariateSpline(hdata,np.array(np.log(data['p'])),k=1, s=0.25) * data['p'].unit
+    Tdata_f = UnivariateSpline(hdata,np.array(np.log(data['T'])),k=1, s=0.25) * data['T'].unit
+    rdata_f = UnivariateSpline(hdata,np.array(np.log(data['rho'])),k=1, s=0.25) * data['rho'].unit
+    #s=0.0 to ensure all points are strictly used for ionisation state
+    muofT_f = UnivariateSpline(hdata,np.array(np.log(data['mu'])),k=1, s=0.0) * u.one
 
+    outdata = Table()
+    outdata['Z'] = Z
+    outdata['p'] = np.exp(pdata_f(Z))
+    outdata['T'] = np.exp(Tdata_f(Z))
+    outdata['rho'] = np.exp(rdata_f(Z))
+    outdata['mu'] = np.exp(muofT_f(Z))
+
+
+    return outdata
 
 
 #----------------------------------------------------------------------------
 # a simpler exponential atmosphere to test Spruit's analytical result
 #----------------------------------------------------------------------------
 def get_spruit_hs(
-                   filenames, 
-                   Z, 
-                   scales, 
+                   filenames,
+                   Z,
+                   scales,
                    model_pars,
                    physical_constants,
                    logical_pars,
                    plot
                  ):
     """ Reference data is collected to satisfy the global call for source data,
-        but only the photospheric values of pressure and density are required 
-        for Spruit. 
+        but only the photospheric values of pressure and density are required
+        for Spruit.
         Four options are available to select Alfven speed along the flux tube
-        axis to be: 
-        constant, increase as the square root of Z, increase linearly and 
-        increase as the square 0f Z. These are approximate due to the effect on 
+        axis to be:
+        constant, increase as the square root of Z, increase linearly and
+        increase as the square 0f Z. These are approximate due to the effect on
         density of the non-zero magnetic tension force.
-    """         
+    """
     logical_pars['l_atmos_val3c_mtw'] = True
     logical_pars['l_spruit'] = False
     pdata_, Tdata_i, rdata_, muofT_i, [val ,mtw] = \
         interpolate_atmosphere(
-                           filenames, 
-                           Z, 
-                           scales, 
+                           filenames,
+                           Z,
+                           scales,
                            model_pars,
                            physical_constants,
                            logical_pars,
                            plot=True
-                          )         
+                          )
     if logical_pars['l_const']:
         pdata_i = 1.1*pdata_[0]\
                      *np.exp(-4.0*Z/model_pars['chrom_scale'])
@@ -174,7 +160,7 @@ def get_spruit_hs(
     muofT_i[:] = physical_constants['mu']
     Tdata_i = pdata_i/rdata_i*physical_constants['mu']\
                              *physical_constants['proton_mass']\
-                             /physical_constants['boltzmann'] 
+                             /physical_constants['boltzmann']
     logical_pars['l_atmos_val3c_mtw'] = False
     logical_pars['l_spruit'] = True
 
@@ -183,18 +169,18 @@ def get_spruit_hs(
 #============================================================================
 # Construct 3D hydrostatic profiles and include the magneto adjustments
 #============================================================================
-def vertical_profile(Zint, Z, 
-                     pdata_i, rdata_i, Tdata_i, muofT_i, 
-                     magp,  
+def vertical_profile(Zint, Z,
+                     pdata_i, rdata_i, Tdata_i, muofT_i,
+                     magp,
                      physical_constants, dz,
                      scales
                     ):
     """Return the vertical profiles for thermal pressure and density in 1D.
        Integrate in reverse from the corona to the photosphere to remove
        sensitivity to larger chromospheric gradients."""
-    
-    g0 = physical_constants['gravity'] 
-    Rgas_v =np.zeros(Z.size)  
+
+    g0 = physical_constants['gravity']
+    Rgas_v =np.zeros(Z.size)
     Rgas_v[:] = physical_constants['boltzmann']/\
                 physical_constants['proton_mass']/muofT_i[4:-4]
     rdata_v = np.zeros(Z.size)
@@ -202,11 +188,11 @@ def vertical_profile(Zint, Z,
     rdata[:] = rdata_i[:]
     #it may be necessary to enhance the density near the footpoint emprically
 #    rdata[:] = rdata_i[:]*(1. + 1.5 * np.exp(-Zint[:]/0.00005)) #+ 1.*rdata_i.min()#+ 0.1*rdata_i[4]*np.exp(-Zint/Z[50])
-    rdata_v[:] = rdata[4:-4] 
+    rdata_v[:] = rdata[4:-4]
     # inverted SAC 4th order derivative scheme to minimise numerical error
-    linp=np.zeros(Z.size) 
+    linp=np.zeros(Z.size)
     """evaluate upper boundary pressure from equation of state + enhancement,
-       magp, which will be replaced by the mean magnetic pressure in the 
+       magp, which will be replaced by the mean magnetic pressure in the
        corona, then integrate from inner next pressure
     """
     linp[-1] = Tdata_i[-5]*rdata_v[-1]*Rgas_v[-1] + magp[-1]
