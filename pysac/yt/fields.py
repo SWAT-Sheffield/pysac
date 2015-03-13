fix the derived field units.
I think this makes is 3.2-dev only

pysac/yt/fields.py
@@ -6,13 +6,9 @@ from yt.frontends.gdf.fields import GDFFieldInfo
 
 class SACGDFFieldInfo(GDFFieldInfo):
     known_other_fields = (
-        ("density", ("g/cm**3", ["density"], None)),
-        ("velocity_x", ("cm/s", ["velocity_x"], None)),
-        ("velocity_y", ("cm/s", ["velocity_y"], None)),
-        ("velocity_z", ("cm/s", ["velocity_z"], None)),
-        ("mag_field_x", ("gauss", ["mag_field_x"], None)),
-        ("mag_field_y", ("gauss", ["mag_field_y"], None)),
-        ("mag_field_z", ("gauss", ["mag_field_z"], None)),)
+        ("velocity_x", ("m/s", ["velocity_x"], None)),
+        ("velocity_y", ("m/s", ["velocity_y"], None)),
+        ("velocity_z", ("m/s", ["velocity_z"], None)))
     known_particle_fields = ()
 
     def __init__(self, *args, **kwargs):
@@ -39,7 +35,7 @@ class SACGDFFieldInfo(GDFFieldInfo):
 
         def internal_energy(field, data):
             return data['internal_energy_pert'] + data['internal_energy_bg']
-        self.add_field(('gas','internal_energy'), function=internal_energy, units='J')
+        self.add_field(('gas','internal_energy'), function=internal_energy, units='Pa')
 
         def mag_pressure(field, data):
             if data.ds.dimensionality == 2:
@@ -48,7 +44,8 @@ class SACGDFFieldInfo(GDFFieldInfo):
             if data.ds.dimensionality == 3:
                 return (data['mag_field_x']**2 + data['mag_field_y']**2 +
                         data['mag_field_z']**2) / (2. * mu_0)
-        self.add_field(('gas','mag_pressure'), function=mag_pressure, units='Pa')
+        self.add_field(('gas','mag_pressure'), function=mag_pressure, units='Pa',
+                       force_override=True)
 
         def thermal_pressure(field, data):
             #p = (\gamma -1) ( e - \rho v^2/2 - B^2/2)
@@ -60,22 +57,27 @@ class SACGDFFieldInfo(GDFFieldInfo):
                 kp = (data['density'] * (data['velocity_x']**2 +
                       data['velocity_y']**2 + data['velocity_z']**2))/2.
             return g1 * (data['internal_energy'] - kp - data['mag_pressure'])
-        self.add_field(('gas','thermal_pressure'), function=thermal_pressure, units='Pa')
+        self.add_field(('gas','thermal_pressure'), function=thermal_pressure,
+                       units='Pa', force_override=True)
 
         def alfven_speed(field, data):
             if data.ds.dimensionality == 2:
-                return np.sqrt(data['mag_field_x']**2 +
-                               data['mag_field_y']**2) / np.sqrt(mu_0 * data['density'])
+                r = np.sqrt(data['mag_field_x']**2 +
+                            data['mag_field_y']**2) / np.sqrt(mu_0 * data['density'])
             if data.ds.dimensionality == 3:
-                return np.sqrt(data['mag_field_x']**2 + data['mag_field_y']**2 +
-                               data['mag_field_z']**2) / np.sqrt(mu_0 * data['density'])
-        self.add_field(('gas', 'alfven_speed'), function=alfven_speed, units=r'm/s**2')
+                r =  np.sqrt(data['mag_field_x']**2 + data['mag_field_y']**2 +
+                             data['mag_field_z']**2) / np.sqrt(mu_0 * data['density'])
+            return r
+        self.add_field(('gas', 'alfven_speed'), function=alfven_speed,
+                       units=r'm/s', force_override=True)
 
         def sound_speed(field, data):
             gamma = data.ds.parameters.get('gamma',[1.66666])[0]
             return np.sqrt((gamma * data['thermal_pressure']) / data['density'])
-        self.add_field(('gas','sound_speed'), function=sound_speed, units=r'm/s**2')
+        self.add_field(('gas','sound_speed'), function=sound_speed,
+                       units=r'm/s', force_override=True)
 
         def plasma_beta(field, data):
             return data['mag_pressure'] / data['thermal_pressure']
-        self.add_field(('gas','plasma_beta'), function=plasma_beta, units=r'dimensionless')
+        self.add_field(('gas','plasma_beta'), function=plasma_beta,
+                       units=r'dimensionless', force_override=True)