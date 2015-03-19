840d6ff8a1fabf0836477eada1e33e52bff14906
Fred <f.gent@sheffield.ac.uk>
2015-03-19 16:16:17 +0000

    Added comments and fixed streamlines to be continuous

--- pysac/mhs_atmosphere/plot/mhs_plot.py
+++ pysac/mhs_atmosphere/plot/mhs_plot.py
@@ -3,6 +3,10 @@
 Created on Tue Feb 17 15:13:23 2015
 
 @author: sm1fg
+
+Use to plot 1D and 2D slices of gdf formatted data. 
+Dictionaries are used so appropriate labels and line colors can be extracted 
+from the data name lists.
 """
 
 import numpy as np
@@ -17,6 +21,10 @@ import matplotlib.colors as colors
 from collections import OrderedDict as od
 import os
 import warnings
+
+#match the field name to the appropriate axis/colorbar labels. Ordered to 
+#control option on, for example, 'mag_pressure'.
+#add to the dictionary if variable not specified
 y_axis_labels = od((
                 ('temp',r'$T$ [K], '),
                 ('dens',r'$\rho$ [kg km$^{-3}$], '),
@@ -29,13 +37,17 @@ y_axis_labels = od((
                 ('beta',r'$\beta$, '),
                 ('alfven',r'$v_\mathrm{A}$ [km s$^{-1}$], '),
                 ('sound',r'$c_\mathrm{s}$ [km s$^{-1}$], ')))
+
+#match the field name to the appropriate line color for consitency between 
+#plots. Ordered to control option on, for example, 'mag_pressure'.
+#add to the dictionary if variable not specified
 line_colours = od((('temp','red'), ('dens','purple'),
                 ('mag_field_x','blue'),
                 ('mag_field_y','blue'),
                 ('mag_field_z','blue'),
                 ('press','green'),
                 ('beta','purple'),('alfven','cyan'),('sound','red')))
-
+#determine line styles and widths between mean profiles and outliers
 line_styles = {
                'min':':',
                'max':':',
@@ -48,23 +60,30 @@ line_widths = {'mean':3.0,
                'edge':3.0
               }
 
+#data may be required from more than one data file for 1D slices, so collate
+#1D slices for use together in plotting. 
+#include min, max, mean, axial and edge profiles.
 def make_1d_slices(ds, var_label, oneD_arrays):
-    """extract 1D arrays from gdf data for plotting
+    """extract 1D arrays from gdf data for plotting.
+    ds: gdf data set, var_label: gdf field name, oneD_arrays: collection of
+    1D slices to which this field's 1D slices shall be appended
     """
     var_ = ds.index.grids[0][var_label]
 
-#    oneD_arrays.create[var_label]
+    #append new 1D slice to existing
     oneD_arrays.update({var_label:{}})
+    #vertical profile along the axis
     oneD_arrays[var_label]['axis'] = var_[ds.domain_dimensions[0]/2,
                                           ds.domain_dimensions[1]/2,:]
+    #vertical profile along the edge of the domain, useful with axial symmetry
     oneD_arrays[var_label]['edge'] = var_[0,        0        ,:]
     oneD_arrays[var_label]['mean'] = np.mean(np.mean(var_, axis=0), axis=0)
     oneD_arrays[var_label]['min' ] = np.min(np.min(var_, axis=0), axis=0)
     oneD_arrays[var_label]['max' ] = np.max(np.max(var_, axis=0), axis=0)
+    #integer size and range of vertical domain may vary so match for each array
     oneD_arrays[var_label]['Z'   ] = np.linspace(ds.domain_left_edge[2],
                                                  ds.domain_right_edge[2],
                                                  ds.domain_dimensions[2])
-
     return oneD_arrays
 
 def make_1d_zplot(f, plot_label,
@@ -74,8 +93,14 @@ def make_1d_zplot(f, plot_label,
                  ylog = True, xlog = False
                 ):
     """select 1D arrays and plot appropriate slices['y',nx_2]
+    f: the collated set of labelled 1D arrays
+    keys: variable's field name
+    subkeys: label discriminating which slice of variable to be plotted with Z
+    figxy: size of figure
+    ylog/xlog: True to use logarithmic scale
     """
     plt.figure(figsize = figxy)
+    #initialise set of tags to be plotted and count for line styles
     tags = set()
     count = 0
     for key in keys:
@@ -97,12 +122,14 @@ def make_1d_zplot(f, plot_label,
             for tag in line_widths.keys():
                 if tag in subkeys:
                     linewidth = line_widths[tag]
+            #rescale density so camparable in plot with pressure/temperature
             rescale = np.string_(f[key][subkey].units)
             if 'density' in key:
                 rescale = 'kg / km**3'
             plt.plot(f[key]['Z'].in_units('Mm'),
                      f[key][subkey].in_units(rescale),color=color,
                      linestyle=linestyle, lw=linewidth, label=label)
+    #collate labels for y-axis and apply log scale if True
     ylabel = ''
     for tag in tags:
         ylabel += y_axis_labels[tag]
@@ -112,10 +139,11 @@ def make_1d_zplot(f, plot_label,
         plt.gca().set_xscale('log',subsy=[5,10])
 #    plt.gca().set_yticks([1.0e-3,1.0e-1,1.0e1,1.0e3,1.0e5])
     plt.xlabel('Height [Mm]')
+    #tidy label string by stripping final ', ' from the end  
     plt.ylabel(ylabel[:-2])
+    #raise plot x-axis to fit x-label 
     plt.subplots_adjust(bottom=0.125)
-#    import pdb; pdb.set_trace()
-#    handles, labels = ax.Axes.get_legend_handles_labels()
+    #consider moving legend for different plots, add a loc to function call?
     legend = plt.legend(loc='center right')
     plt.savefig(plot_label)
 
@@ -128,6 +156,7 @@ def make_2d_plot(ds, var_field, figname, normal = ['y',64],
     string var_field is selected. 
     figname: name derived from the model name, variable name and directory 
     into which the plot will be saved.
+    normal: specifies orientation of plane and which slice from 0 to N-1) 
     options allow magnetic field lines to be overplotted and/or plasma beta
     contours.
     """
@@ -139,6 +168,7 @@ def make_2d_plot(ds, var_field, figname, normal = ['y',64],
 
     #select the 2D slice from the 3D array (normal[0]: direction of plane
     #and normal[1] the integer index of the slice selected)
+    #extent: range of plot domain
     if normal[0] is 'x':
         slc = var[normal[1],:,:]/var.unit_quantity
         extent=[ds.domain_left_edge[1].in_units('Mm'),
@@ -168,6 +198,8 @@ def make_2d_plot(ds, var_field, figname, normal = ['y',64],
         colour = cm.PiYG_r
     if 'tension' in var_field:
         colour = cm.RdYlGn_r
+    if 'velocity' in var_field:
+        colour = cm.RdYlGn
 
     #increase the horizontal extent to include second colorbar for field lines
     if lines:
@@ -175,7 +207,7 @@ def make_2d_plot(ds, var_field, figname, normal = ['y',64],
         figxz = [xz2,figxz[1]]
     plt.figure(figsize=figxz)
 
-    #adjust color scaling for variable profiles
+    #adjust color scaling appropriate for field profiles
     if slc.min() < 0:
         norm = colors.SymLogNorm(slc.max())
         l_norm = True
@@ -193,17 +225,20 @@ def make_2d_plot(ds, var_field, figname, normal = ['y',64],
             else:
                 l_norm = False
 
-    #plot a 2D 
+    #plot a 2D slice
     plt.imshow(slc.T, colour,
-    extent=extent,
-    norm=norm,
-    origin='lower',
-    vmin=slc.min(),
-    vmax=slc.max())
+               extent=extent,
+               norm=norm,
+               origin='lower',
+               vmin=slc.min(),
+               vmax=slc.max()
+              )
     plt.axes().set_aspect(aspect=aspect)
     plt.ylabel('Height [Mm]')
     plt.xlabel('Width [Mm]')
-    plt.autoscale(False)
+    plt.autoscale(False) # fix plot to extent
+
+    # add field's colorbar with appropriate label
     if l_norm:
         l_f = LogFormatterMathtext(10, labelOnlyBase=False)
     else:
@@ -217,12 +252,14 @@ def make_2d_plot(ds, var_field, figname, normal = ['y',64],
                         fraction=0.2)
     cbar1.ax.set_ylabel(ylabel)
     cbar1.solids.set_edgecolor("face")
+
     # obtain data arrays for beta contours and magnetic field lines and save
     # for use in plots for other variables - magnetic field must data must be
-    # included in first run.
+    # included in gdf file of first run.
     if lines or contours:
         warnings.warn("magnetic field data required for first run to obtain"+
                       "field lines or Beta contours", Warning)
+        #set up files where magnetic and beta slices will be stored
         if len(model) == 0:
             raise ValueError("in plot.mhs_plot.make_2d_plot specify the model")
         flinesdir = os.path.expanduser('~/Documents/mhs_atmosphere/flines/')
@@ -234,7 +271,8 @@ def make_2d_plot(ds, var_field, figname, normal = ['y',64],
                            str(normal[1])+'_'+model+'.npy'
         beta_file = flinesdir+'beta_norm_'+normal[0]+'_slice_'+\
                            str(normal[1])+'_'+model+'.npy'
-        if 'mag_field_x_bg' not in np.array(ds.field_list)[:,1]:
+        #acquire the B field components and beta for the lines and contours
+        if ('gdf','mag_field_x_bg') not in ds.field_list:
             v1 = np.load(v1_file)
             v2 = np.load(v2_file)
             beta = np.load(beta_file)
@@ -254,23 +292,33 @@ def make_2d_plot(ds, var_field, figname, normal = ['y',64],
             v1 = np.array(v1)
             v2 = np.array(v2)
             beta = np.array(beta)
+            #save B and beta for use with other gdf data sets
             np.save(v1_file, v1)
             np.save(v2_file, v2)
             np.save(beta_file, beta)
+        #plot field lines as streamlines with color and thickness functions of
+        #field magnitude
+        """Consider replacing strength from 2D arrays with actual derived field
+        of 'mag_magnitude' saved as fourth data set speed?
+        Also include option to apply velocity and alternative to beta: FAG
+        """
         if lines:
             clabel = y_axis_labels['mag_strength'][:-2]
             X = np.linspace(extent[0],extent[1],slc.shape[0])       
             Y = np.linspace(extent[2],extent[3],slc.shape[1])
             U = np.sqrt(v1**2 + v2**2)
             plt.streamplot(X.value, Y.value, v1.T, v2.T, color=U.T,
-                           cmap=cm.BuPu, density=0.5, 
-                           linewidth = 2*np.exp(np.log(U.T)-np.log(U.max()))
+                           cmap=cm.BuPu, density=0.9, 
+                           linewidth = 3*U.T/U.max(), minlength = 0.75,
+                           #linewidth = 2*np.exp(np.log(U.T)-np.log(U.max()))
                           ) 
+            #add second colorbar for field strength 
             cbar2 = plt.colorbar(format = l_f,
                                #ticks=[var.min(),1,10,100,1000,var.max()],
                                 fraction=0.2)
             cbar2.ax.set_ylabel(clabel)
             cbar2.solids.set_edgecolor("face")
+        # plot contours with inline labels 
         if contours:
             X = np.linspace(extent[0],extent[1],slc.shape[0])       
             Y = np.linspace(extent[2],extent[3],slc.shape[1])
@@ -287,98 +335,100 @@ def make_2d_plot(ds, var_field, figname, normal = ['y',64],
                 fomt = '%.1f'
             else:
                 fomt = '%.0f'
-#            import pdb; pdb.set_trace()
             plt.clabel(CS, fmt=fomt)
-#    import pdb; pdb.set_trace()
     plt.savefig(figname)
+#    import pdb; pdb.set_trace()
 
-#============================================================================
-# Fieldline Generation
-#============================================================================
-#def get_seeds(model):
-    
-
-
-def make_field_lines(v1,v2,seeds,extent,flines_file,dS=1):
-    """ Simple Euler fieldline integrator
-    v1,v2 - y and x vectors*lscale
-    seeds - array of coordinate (array indexes) pairs
-    dS [optional] - step size
-    """
-    from scipy import ndimage
-    
-    field = []
-    #Get extent of domain
-    max1 = v1.shape[0]
-    max2 = v2.shape[1]
-    min2 = 0
-    min1 = 0
-    vmax = np.sqrt(np.max(v1**2+v2**2))/v1.unit_quantity
-    #integrate for each fieldline
-    for seed1 in seeds[1]:
-        for seed0 in seeds[0]:
-            c1,c2 = seed0,seed1
-            out1 = [c1] #first point is seed
-            out2 = [c2]
-            coords = np.array([[c1],[c2]])
-            v1i = ndimage.map_coordinates(v1,coords)[0]
-            v2i = ndimage.map_coordinates(v2,coords)[0]
-            out3 = [np.sqrt(v1i**2 + v2i**2)/vmax]
-            cnt = 0
-            while (c1 <= max1-0.5 and c1 >= min1+0.5) and (c2 <= max2-0.5
-                                                    and c2 >= min2+0.25):
-                #Interpolate the vector field to the coord downwards
-                coords = np.array([[c1],[c2]])
-                v1i = ndimage.map_coordinates(v1,coords)[0]
-                v2i = ndimage.map_coordinates(v2,coords)[0]
-                vi = np.sqrt(v1i**2 + v2i**2)
-                d1 = ( v1i * dS )/ vi
-                d2 = ( v2i * dS )/ vi
-                c1 -= d1
-                c2 -= d2
-                c3 = vi/vmax
-                out1.append(c1)
-                out2.append(c2)
-                out3.append(c3)
-                cnt += 1
-                if cnt > max2*5:
-                    print "limit seeds = ",seed0,seed1
-                    break
-#            out = np.zeros([len(out1),len(out2),len(out3)])
+##============================================================================
+## Fieldline Generation
+##============================================================================
+##def get_seeds(model):
+"""Depricated version to calculate field lines, subsequent to inbuilt 
+streamlines with color and thickness analogue
+"""
+#    
+#
+#
+#def make_field_lines(v1,v2,seeds,extent,flines_file,dS=1):
+#    """ Simple Euler fieldline integrator
+#    v1,v2 - y and x vectors*lscale
+#    seeds - array of coordinate (array indexes) pairs
+#    dS [optional] - step size
+#    """
+#    from scipy import ndimage
+#    
+#    field = []
+#    #Get extent of domain
+#    max1 = v1.shape[0]
+#    max2 = v2.shape[1]
+#    min2 = 0
+#    min1 = 0
+#    vmax = np.sqrt(np.max(v1**2+v2**2))/v1.unit_quantity
+#    #integrate for each fieldline
+#    for seed1 in seeds[1]:
+#        for seed0 in seeds[0]:
+#            c1,c2 = seed0,seed1
+#            out1 = [c1] #first point is seed
+#            out2 = [c2]
+#            coords = np.array([[c1],[c2]])
+#            v1i = ndimage.map_coordinates(v1,coords)[0]
+#            v2i = ndimage.map_coordinates(v2,coords)[0]
+#            out3 = [np.sqrt(v1i**2 + v2i**2)/vmax]
+#            cnt = 0
+#            while (c1 <= max1-0.5 and c1 >= min1+0.5) and (c2 <= max2-0.5
+#                                                    and c2 >= min2+0.25):
+#                #Interpolate the vector field to the coord downwards
+#                coords = np.array([[c1],[c2]])
+#                v1i = ndimage.map_coordinates(v1,coords)[0]
+#                v2i = ndimage.map_coordinates(v2,coords)[0]
+#                vi = np.sqrt(v1i**2 + v2i**2)
+#                d1 = ( v1i * dS )/ vi
+#                d2 = ( v2i * dS )/ vi
+#                c1 -= d1
+#                c2 -= d2
+#                c3 = vi/vmax
+#                out1.append(c1)
+#                out2.append(c2)
+#                out3.append(c3)
+#                cnt += 1
+#                if cnt > max2*5:
+#                    print "limit seeds = ",seed0,seed1
+#                    break
+##            out = np.zeros([len(out1),len(out2),len(out3)])
+##            out[0] = out1 * (extent[1] - extent[0])/(max1-1) + extent[0]
+##            out[1] = out2 * (extent[3] - extent[2])/(max2-1) + extent[2]
+##            out[2] = out3
+##            field.append(out)
+#            c1,c2 = seed0,seed1
+#            cnt = 0
+#            while (c1 <= max1-0.5 and c1 >= min1+0.5) and (c2 <= max2-0.5
+#                                                    and c2 >= min2+0.25):
+#                #Interpolate the vector field to the coord upwards
+#                coords = np.array([[c1],[c2]])
+#                v1i = ndimage.map_coordinates(v1,coords)[0]
+#                v2i = ndimage.map_coordinates(v2,coords)[0]
+#                vi = np.sqrt(v1i**2 + v2i**2)
+#                d1 = ( v1i * dS )/ vi
+#                d2 = ( v2i * dS )/ vi
+#                c1 += d1
+#                c2 += d2
+#                c3 = vi/vmax
+#                out1.append(c1)
+#                out2.append(c2)
+#                out3.append(c3)
+#                cnt += 1
+#                if cnt > max2*5:
+#                    print "limit seeds = ",seed0,seed1
+#                    break
+#            out = np.zeros([3,len(out1)])
 #            out[0] = out1 * (extent[1] - extent[0])/(max1-1) + extent[0]
 #            out[1] = out2 * (extent[3] - extent[2])/(max2-1) + extent[2]
 #            out[2] = out3
 #            field.append(out)
-            c1,c2 = seed0,seed1
-            cnt = 0
-            while (c1 <= max1-0.5 and c1 >= min1+0.5) and (c2 <= max2-0.5
-                                                    and c2 >= min2+0.25):
-                #Interpolate the vector field to the coord upwards
-                coords = np.array([[c1],[c2]])
-                v1i = ndimage.map_coordinates(v1,coords)[0]
-                v2i = ndimage.map_coordinates(v2,coords)[0]
-                vi = np.sqrt(v1i**2 + v2i**2)
-                d1 = ( v1i * dS )/ vi
-                d2 = ( v2i * dS )/ vi
-                c1 += d1
-                c2 += d2
-                c3 = vi/vmax
-                out1.append(c1)
-                out2.append(c2)
-                out3.append(c3)
-                cnt += 1
-                if cnt > max2*5:
-                    print "limit seeds = ",seed0,seed1
-                    break
-            out = np.zeros([3,len(out1)])
-            out[0] = out1 * (extent[1] - extent[0])/(max1-1) + extent[0]
-            out[1] = out2 * (extent[3] - extent[2])/(max2-1) + extent[2]
-            out[2] = out3
-            field.append(out)
-#    import pdb; pdb.set_trace()
-    np.save(flines_file, field)        
-
-    return np.array(field)
+##    import pdb; pdb.set_trace()
+#    np.save(flines_file, field)        
+#
+#    return np.array(field)
 
 #============================================================================
 # Fieldline Generation
