afc2b8baea941d2f980157e83f5cbb21b13f1866
Frederick Gent <fred.gent.ncl@gmail.com>
2015-04-08 15:08:33 +0100

    corrected B0z from quadratic to exponential

--- pysac/mhs_atmosphere/parameters/options.py
+++ pysac/mhs_atmosphere/parameters/options.py
@@ -43,7 +43,7 @@ def set_options(model, l_mpi, l_gdf=True):
     if model['model'] == 'mfe_setup':
         option_pars['l_single']          = True 
         option_pars['l_mfe']             = True 
-        option_pars['l_B0_quadz']        = True 
+        option_pars['l_B0_expz']        = True 
         option_pars['l_atmos_val3c_mtw'] = True 
     if model['model'] == 'spruit':    
         option_pars['l_single']          = True 
