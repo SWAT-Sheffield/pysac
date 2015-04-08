4ec8741570973ab3e1cdfc0b9968c7c7f219a510
Frederick Gent <fred.gent.ncl@gmail.com>
2015-04-08 15:18:21 +0100

    Revising seeds for magnetic field to use mhs_model parameters

--- examples/mhs_atmosphere/plots/val_mtw_plot_3d.py
+++ examples/mhs_atmosphere/plots/val_mtw_plot_3d.py
@@ -8,7 +8,8 @@ import os
 import numpy as np
 import pysac.yt as sacyt
 import pysac.mhs_atmosphere as atm
-from pysac.mhs_atmosphere.parameters.model_pars import paper2a as model_pars
+from pysac.mhs_atmosphere.parameters.model_pars import paper1 as model_pars
+import astropy.units as u
 #papers = ['paper1','paper2a','paper2b','paper2c','paper2d','mfe_setup'
 
 l_mpi=False
@@ -27,21 +28,20 @@ fkeys = ['density','alfven_speed',
 option_pars = atm.set_options(model_pars, l_mpi, l_gdf=True)
 coords = atm.get_coords(model_pars['Nxyz'], u.Quantity(model_pars['xyz']))
 xc, yc, S = atm.get_flux_tubes(model_pars, coords, option_pars)
+maxr = ds.domain_right_edge[1].in_units('Mm').value
+maxz = ds.domain_right_edge[2].in_units('Mm').value
+nrad, nangle = 7, 4
+seeds = [[0.,0.,maxz]]
+for ix in xc:
+    for ti,r in enumerate(maxr*(1.01-np.exp(-(np.linspace(0, 2, nrad))**2))):
+        for theta in np.linspace(0, 2 * np.pi, nangle, endpoint=False):
+            seeds.append([r * np.cos(theta + r + 0.5 * ti) + xc[0].value,
+                          r * np.sin(theta + r + 0.5 * ti) + yc[0].value, maxz])
+    seeds = np.array(seeds)
 
 if 'paper1' in file_:
     figxy = [500,550]
-    view = (-45.0, 90.0, 22.0, np.array([0,0,4.25]))
-    xc = 0
-    yc = 0
-    maxr = ds.domain_right_edge[1].in_units('Mm').value
-    maxz = ds.domain_right_edge[2].in_units('Mm').value
-    nrad, nangle = 7 , 6
-    seeds = [[xc,yc,maxr]]
-    for ti,r in enumerate(maxr*(1-np.exp(-(np.linspace(0, 1, nrad))**2))):
-        for theta in np.linspace(0, 2 * np.pi, nangle, endpoint=False):
-            seeds.append([r * np.cos(theta + 0.5 * ti) + xc,
-                          r * np.sin(theta + 0.5 * ti) + yc, maxz])
-    seeds = np.array(seeds)
+    view = (-45.0, 90.0, 22.0, np.array([0,0,4.25]))   
 if 'paper2a' in file_:
     figxy = [500,750]
     view = (-45.0, 90.0, 20.0, np.array([0,0,3.75]))
